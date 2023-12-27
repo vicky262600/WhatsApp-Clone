1545ac6d28da7d0e1b60f4af5b51f659865728a7
vicky262600 <vickyjoshi2604@gmail.com>
2023-12-27 11:32:25 -0500

    all done

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/firestore';
-// import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 // const firebaseConfig = {
 //   apiKey: "AIzaSyCHuLBzBaPdDu6ygJZZzF3h025BrEb19Lo",
@@ -15,19 +15,37 @@ import 'firebase/firestore';
 // };
 
 
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+// import { initializeApp } from 'firebase/app';
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import { getFirestore } from 'firebase/firestore';
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCHuLBzBaPdDu6ygJZZzF3h025BrEb19Lo",
+//   authDomain: "whatsapp-clone-1dbed.firebaseapp.com",
+//   projectId: "whatsapp-clone-1dbed",
+//   storageBucket: "whatsapp-clone-1dbed.appspot.com",
+//   messagingSenderId: "1043910258272",
+//   appId: "1:1043910258272:web:ea6bc0e7fbff4a01f19d05",
+//   measurementId: "G-LPSXZNSCY0",
+// };
+
+// const firebaseApp = initializeApp(firebaseConfig);
 // const auth = getAuth(firebaseApp);
-// const db = firebaseApp.firestore();
-// auth.languageCode = 'it';
-// const provider = new firebase.auth.GoogleAuthProvider();
+// const db = getFirestore(firebaseApp);
+// const provider = new GoogleAuthProvider();
 
 // export { auth, provider };
 // export default db;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCHuLBzBaPdDu6ygJZZzF3h025BrEb19Lo",
   authDomain: "whatsapp-clone-1dbed.firebaseapp.com",
@@ -35,13 +53,17 @@ const firebaseConfig = {
   storageBucket: "whatsapp-clone-1dbed.appspot.com",
   messagingSenderId: "1043910258272",
   appId: "1:1043910258272:web:ea6bc0e7fbff4a01f19d05",
-  measurementId: "G-LPSXZNSCY0",
+  measurementId: "G-LPSXZNSCY0"
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
-const provider = new GoogleAuthProvider();
+const db = firebaseApp.firestore();
+auth.languageCode = 'it';
+const provider = new firebase.auth.GoogleAuthProvider();
 
 export { auth, provider };
 export default db;
